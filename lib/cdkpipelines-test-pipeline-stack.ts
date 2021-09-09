cc4692f2f9d8338f5a3d99e28dbc9dd10a015361
Juba Cochran <jubacochran@gmail.com>
2021-09-09 15:47:55 +0200

    fixing pipeline stack var

--- lib/cdkpipelines-test-pipeline-stack.ts
+++ lib/cdkpipelines-test-pipeline-stack.ts
@@ -15,7 +15,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
        // How it will be built and synthesized
        synth: new ShellStep('Synth', {
          // Where the source can be found
-         input: CodePipelineSource.gitHub('jubacochran/cdkpipeline', 'main'),
+         input: CodePipelineSource.gitHub('jubacochran/cdkpipeline', 'master'),
          
          // Install dependencies, build and run cdk synth
          commands: [
